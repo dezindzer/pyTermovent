b1ec9a0e5859f292fa1f91822877362f252087a0
尼古拉 <djindjojlo@gmail.com>
2025-08-26 12:56:34 +0200

    updater updated

--- pyTermovent.tab/Other.panel/Update.pushbutton/script.py
+++ pyTermovent.tab/Other.panel/Update.pushbutton/script.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
-from pyrevit import script, forms, extensions
-from pyrevit import updater
+from pyrevit import forms, extensions, updater
 
 # Name of your extension
 extension_name = "pyTermovent"
 
 # Find the extension
-ext = next((e for e in extensions.get_extensions() if e.name == extension_name), None)
+ext = None
+for e in extensions.get_extensions():
+    if e.name == extension_name:
+        ext = e
+        break
 
+#pyRevit uses IronPython 2.7
 if ext:
-    updater.update_extension(ext.path)
-    forms.alert(f"Extension '{extension_name}' updated!", title="Update Complete")
+    updater.update_extension(ext)
+    forms.alert("Extension '{}' updated!".format(extension_name), title="Update Complete")
 else:
-    forms.alert(f"Extension '{extension_name}' not found.", title="Error")
+    forms.alert("Extension '{}' not found.".format(extension_name), title="Error")
+    
+#pyRevit uses IronPython 3
+# if ext:
+#     updater.update_extension(ext)  # pass the extension object
+#     forms.alert(f"Extension '{extension_name}' updated!", title="Update Complete")
+# else:
+#     forms.alert(f"Extension '{extension_name}' not found.", title="Error")
